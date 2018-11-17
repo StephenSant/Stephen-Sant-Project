35090b26a412f1bc0404fbebcd05cf89a1c0501e
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-11-17 12:37:57 +1100

    17/11/18

---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    [HideInInspector]
+    public bool showOptions;
+    [Header("References")]
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+	void Start ()
+    {
+        mainMenu = GameObject.Find("Main Menu Panel");
+        optionsMenu = GameObject.Find("Options Panel");
+        optionsMenu.SetActive(false);
+        showOptions = false;
+    }
+
+    public void NewGame(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void GoToScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ToggleOptions()
+    {
+        if (!showOptions)
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+            showOptions = true;
+        }
+        else if (showOptions)
+        {
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            showOptions = false;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/OptionsHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+public class OptionPrefs
+{
+    public float volume;
+    public Vector2 resolution;
+}
+
+public class OptionsHandler : MonoBehaviour
+{
+    public float volume;
+    public Vector2[] res = new Vector2[7];
+    public int resIndex;
+    public bool isFullScreen = true;
+    public List<KeyCode> keysBinds;
+
+    [Header("References")]
+    public Slider volSlider;
+    public Dropdown resDropdown;
+    public Toggle windowedToggle;
+    public List<Button> keyBindButton;
+
+    [Header("Game Saves")]
+    public string fileName = "OptionPrefs";
+    private OptionPrefs optionsData = new OptionPrefs();
+    private string fullPath;
+
+    private void Awake()
+    {
+        fullPath = Application.dataPath + "/GameData/" + fileName + ".xml";
+    }
+
+    private void Start()
+    {
+        volSlider.value = volume;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y,!isFullScreen);
+    }
+
+    private void Update()
+    {
+        volume = volSlider.value;
+    }
+    public void Resolution()
+    {
+        resIndex = resDropdown.value;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+    }
+    public void Windowed()
+    {
+        isFullScreen = windowedToggle.isOn;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+    }
+    public void SetKeyBinds()
+    {
+
+    }
+    public void SavePrefs()
+    {
+        optionsData.volume = volume;
+        optionsData.resolution = res[resIndex];
+        var serializer = new XmlSerializer(typeof(OptionPrefs));
+        using (var stream = new FileStream(fullPath, FileMode.Create))
+        {
+            serializer.Serialize(stream, optionsData);
+        }
+    }
+}
